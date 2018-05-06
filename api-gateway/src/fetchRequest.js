74a8a877e811fe656a56fce4ffe466a8c9ff29d9
Michał Sypetkowski <M.Sypetkowski@gmail.com>
2018-05-06 14:29:52 +0200

    #3 Modify api-gateway for integration with beer-service.

--- api-gateway/src/fetchRequest.js
+++ api-gateway/src/fetchRequest.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch')
+const querystring = require('query-string')
 
 const { restEndpoint } = require('./restEndpoint')
 const { hosts } = require('./properties')
@@ -6,10 +7,14 @@ const { hosts } = require('./properties')
 exports.fetchRequest = restEndpoint(async ctx => {
   const uri = prepareUri(ctx)
   const data = prepareData(ctx)
+
+  console.log("DATA", data)
+  console.log("URI", uri)
   const res = await fetch(uri, data)
     .then(res => res.text())
     .catch(error => console.log('FETCH ERROR:',error) && null)
-    return res
+  console.log("RES", res)
+  return res
 })
 
 function prepareUri(ctx) {
@@ -20,18 +25,20 @@ function prepareUri(ctx) {
 function prepareData(ctx) {
   const data = {
     method: ctx.method,
-    headers: prepareHeaders(ctx)
+    // headers: prepareHeaders(ctx)
   }
   if (ctx.method != "GET" && ctx.method != "HEAD") {
-    data.body = JSON.stringify(ctx.request.body)
+    body = ctx.request.body
+    body.userId = ctx.headers.userId
+    data.body = querystring.stringify(body)
   }
   return data
 }
 
-function prepareHeaders(ctx) {
-  return {
-    cookie: ctx.headers.cookie,
-    userId: ctx.headers.userId,
-    'Content-Type': 'application/json'
-  }
-}
+// function prepareHeaders(ctx) {
+//   return {
+//     cookie: ctx.headers.cookie,
+//     userId: ctx.headers.userId,
+//     'Content-Type': 'application/json'
+//   }
+// }
