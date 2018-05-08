175c4914b05b30ca3b71f5578a5b0d30ad9c1e5b
mstar95 <m.star@wp.pl>
2018-05-08 21:58:21 +0200

    test-service removed

--- api-gateway/src/properties.js
+++ api-gateway/src/properties.js
@@ -1,12 +1,10 @@
 const DEFAULT_HOST = 'localhost'
 const DEFAULT_PORT = ':8080'
 
-testService = (process.env.testService || DEFAULT_HOST) + DEFAULT_PORT
 authService = (process.env.authService || DEFAULT_HOST) + DEFAULT_PORT
 beerService = (process.env.beerService || DEFAULT_HOST) + ':8082'
 
 exports.hosts = {
   auth: authService,
-  test: testService,
   beer: beerService,
 }
