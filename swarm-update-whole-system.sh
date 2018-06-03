1a418ffa01c640caae0125f55e4bb5b294bc9d9f
Zwirek009 <maciek.wiraszka@gmail.com>
2018-06-03 19:17:06 +0200

    #25: change swarm update script

--- swarm-update-whole-system.sh
+++ swarm-update-whole-system.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 docker stack rm rso-swarm
 timeout 20s
+docker pull zwirek009/rsoproject_api-gateway:latest
+docker pull zwirek009/rsoproject_auth-service:latest
+docker pull zwirek009/rsoproject_beer-service:latest
+docker pull redis
+docker pull zwirek009/rsoproject_frontend:latest
 docker stack deploy -c docker-compose-swarm.yml rso-swarm
